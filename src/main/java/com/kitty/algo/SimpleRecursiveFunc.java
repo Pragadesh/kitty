d2c23b1d002a6d78fdd3e6c179c703dd3556be8c
pgopalakrishnan <pgopalakrishnan@ebay.com>
2016-08-27 22:30:15 -0500

    simple recursive exampple

--- src/main/java/com/kitty/algo/SimpleRecursiveFunc.java
+++ src/main/java/com/kitty/algo/SimpleRecursiveFunc.java
@@ -3,10 +3,16 @@ package com.kitty.algo;
 public class SimpleRecursiveFunc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n=3;
+		int value = func1(n);
+		System.out.println(String.format("Function value for %d is %d.", n, value));
 	}
 	
-	
+	public static int func1(int n){
+		if(n==0) return 0;
+		int value = (n*n) + 2*func1(n-1);
+		System.out.println(String.format("Return value for %d is %d.", n, value));
+		return value;
+	}
 
 }
