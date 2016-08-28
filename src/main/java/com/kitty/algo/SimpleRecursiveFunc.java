de7da24703ca30e32833452bc7ee1581115c618a
pgopalakrishnan <pgopalakrishnan@ebay.com>
2016-08-28 06:45:19 -0700

    printDigit

--- src/main/java/com/kitty/algo/SimpleRecursiveFunc.java
+++ src/main/java/com/kitty/algo/SimpleRecursiveFunc.java
@@ -3,16 +3,20 @@ package com.kitty.algo;
 public class SimpleRecursiveFunc {
 
 	public static void main(String[] args) {
-		int n=3;
-		int value = func1(n);
-		System.out.println(String.format("Function value for %d is %d.", n, value));
+		int n=12903;
+		printDigit(n);
 	}
 	
 	public static int func1(int n){
 		if(n==0) return 0;
 		int value = (n*n) + 2*func1(n-1);
-		System.out.println(String.format("Return value for %d is %d.", n, value));
 		return value;
+	}
+	
+	public static void printDigit(int n){
+		if(n>=10)
+			printDigit(n/10);
+		System.out.print(n%10);
 	}
 
 }
